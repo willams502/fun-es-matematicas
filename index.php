fa5422b193037b9255421f176605bbd7fd92a983
Willams <84189241+willams502@users.noreply.github.com>
2022-07-16 12:17:54 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+    //FUNÇÕES MATEMATICAS
+    // min() pi() max() abs(valor absoluto positivo) sqrt(raiz quadrada) round(sempre aredonda o número para cima)  rand(gera um número random)
+    $numeros = [1,10,40,100,70,981]; /*array pode fazer com []tambem*/
+    $abs = -20;
+    $raiz =  38;
+    $aren = 1.65;
+    //echo min($numeros);
+
+?>
